a034ad5768dbe7e0170a4e2bb88ee2e56639eef5
Nicolas <nalfonso@student.42lisboa.com>
2026-06-28 16:14:57 +0100

    Implementation of the structure od C00 and the  all CPP Modules , Included, finish and test it ex00 of module 1

---
+++ C00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+
+# include <iostream>
+
+
+int main(int ac, char **av)
+{
+	if (ac == 1) 
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+		{
+			char c = av[i][j];
+			if (c >= 'a' && c <= 'z')
+				c -= 32;
+			std::cout << c;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
+
+// Logic 
+
+// If the # of arguments is equal to 0. I need to return 
+// * LOUD AND UNBEARABLE FEEDBACK NOISE *
+
+// It doesnt matter th number of arguments i have i need to print all in upper case
+// Forthis step ican create a function that allow me to make this 
